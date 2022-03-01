662e0ed78de22156a6fa80f723108679ba37f451
felipeguasti <86924654+felipeguasti@users.noreply.github.com>
2022-03-01 19:23:07 -0300

    Publish new post and flash messages
    Now, It's possible to sent a new post to the DB, the single post page was created, but I still neet to make it to recover the data from the DB and display. A flash message was created to pop up the status of the posting.

--- app/Main.js
+++ app/Main.js
@@ -1,20 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
 // My Componenets
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
+  const [flashMessages, setFlashMessages] = useState([])
+
+  function addFlashMessage(msg) {
+    setFlashMessages(prev => prev.concat(msg))
+  }
+
   return (
     <BrowserRouter>
-      <Header />
+      <FlashMessages messages={flashMessages} />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<HomeGuest />} />
+        <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
+        <Route path="/create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
