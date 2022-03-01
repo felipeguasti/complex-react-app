b9efe8b2d17f623a2d4e48518caa87478024bcf0
felipeguasti <86924654+felipeguasti@users.noreply.github.com>
2022-03-01 09:49:38 -0300

    The layout and interface
    The interface and layout are ready. The app is already communicating with the backend.

---
+++ app/Main.js
@@ -0,0 +1,30 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// My Componenets
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
